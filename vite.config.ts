2a27934099facef97eba854fc3a2aa73b1ee37ac
Sherlock Luk <sherlockluk@outlook.com>
2025-05-23 12:18:09 +0100

    Prepare for deploying on CF Workers

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  plugins: [solidPlugin(), tailwindcss()],
+  plugins: [solidPlugin(), tailwindcss(), cloudflare()],
   server: {
     port: 3000,
   },
